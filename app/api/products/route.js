47b9bc655887d42d69141d70eb4e4a2363de39ef
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2026-05-16 12:04:07 +0700

    Update code

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -7,6 +7,49 @@ const baseProducts = [
     price: "Rp. 550.000",
     label: ["Sale", "Jacket"],
     image: "/img/products/O2H_Hostel_Navy_Half-Zip_Jacket/1.png",
+    deskripsi: `
+      Long sleeve
+
+      Sizing Measurement tshirt :
+      (Length x Width)
+      Size 1: 65cm X 58cm
+      Size 2: 67cm x 60cm
+      Size 3: 69cm x 62cm
+      (toleransi size 1-2cm)
+      
+      Material:
+      Cotton Combed 20s Supersoft
+      Sablon
+
+      Wajib Dibaca!
+      Membeli = Setuju.
+      
+      Wajib Melakukan Video Unboxing Barang Untuk Memastikan Apabila Produk Kami Cacat Atau Ada Kesalahan Pengiriman
+      Cara Order Barang
+      - Barang Yang Kami Kirimkan Hanya Yang Kaka Order Sesuai Dari Sistem Bukan Melalui Notes Catatan Atau Chat.
+      - Pastikan No Handphone Dan Alamat Sudah Sesuai, Agar Barang Bisa Sampai Ke Tempat Kaka Tanpa Ada Masalah.
+      
+      Pembatalan Barang 
+      - Apabila Ada Pergantian Warna Atau Ukuran, Mohon Ajukan Batal Apabila Tidak Akan Tetap Dikirimkan Sesuai Pesanan KakaDan Tidak Berlaku Bila No Resi Proses Sudah Keluar.
+      
+      Ketersediaan Barang :
+      - Selama size dan warna bisa dipilih berarti barang ready dan siap kirim.- Apabila barang ternyata kosong, akan kami infokan secepatnya
+      
+      Jadwal Pengiriman:
+      - Senin s/d Jumat Maksimal transaksi pukul 15.00 WIB. Lebih dari itu masuk proses kirim besok
+      - Estimasi tiba Pengiriman Barang Tergantung Ekspedisi Dan Tanggung Jawab Ekspedisi
+      
+      Keluhan Barang :
+      - Bintang 1 Tidak Akan Menyelesaikan Masalah, Apabila Ada Produk Kami Yang Bermasalah, Salah Kirim Atau CacatMohon Diinfokan Kepada Kami Langsung Via Chat Dengan Bukti Yang Ada.
+      
+      Penukaran Barang :
+      - Tidak menerima penukaran barang, terkecuali dari kita memang ada barang cacat atau kesalahan kirim Return / Komplain Produk. Kalau ada produk atau orderan terjadi kendala seperti halnya:
+      Produk cacat
+      Produk kurang lengkap
+      Produk tidak sesuai deskripsi
+      
+      Salah kirim produk (termasuk salah size)bisa ditanyakan terlebih dahulu via personal chat toko dan juga SERTAKAN bukti video unboxing dan foto saat barang diterima dari kurir. Jikalau 2 syarat ini tidak terpenuhi kita ga bisa proses apapun yang terjadi. Jadi di mohon dengan sangat untuk di simak yah!!.
+    `,
     sold: "10RB+ Terjual",
     slug: "o2h-hostle-navy-half-zip-jacket",
   },
@@ -18,6 +61,55 @@ const baseProducts = [
       "/img/products/O2H_NFL_Black_Jersey/1.png",
       "/img/products/O2H_NFL_Black_Jersey/2.png",
     ],
+    deskripsi: `
+      Sizing Measurement
+      (Panjang x Lebar)
+
+      Size I: 70cm x 52cm
+      Size II: 72cm x 54cm
+      Size III: 74cm x 56cm
+      
+      Material:
+      Mesh
+      Microfabric
+      Bordir
+      
+      Wajib Dibaca! Membeli = Setuju.
+
+      Wajib Melakukan Video Unboxing Barang Untuk Memastikan Apabila Produk Kami Cacat Atau Ada Kesalahan Pengiriman
+
+      Cara Order Barang :
+      - Barang Yang Kami Kirimkan Hanya Yang Kaka Order Sesuai Dari Sistem Bukan Melalui Notes Catatan Atau Chat.
+      - Pastikan No Handphone Dan Alamat Sudah Sesuai, Agar Barang Bisa Sampai Ke Tempat Kaka Tanpa Ada Masalah.
+      
+      Pembatalan Barang :
+      - Apabila Ada Pergantian Warna Atau Ukuran, Mohon Ajukan Batal Apabila Tidak Akan Tetap Dikirimkan Sesuai Pesanan Kaka
+      Dan Tidak Berlaku Bila No Resi Proses Sudah Keluar.
+      
+      Ketersediaan Barang :
+      - Selama size dan warna bisa dipilih berarti barang ready dan siap kirim.
+      - Apabila barang ternyata kosong, akan kami infokan secepatnya
+      
+      Jadwal Pengiriman:
+      - Senin s/d Jumat Maksimal transaksi pukul 15.00 WIB. Lebih dari itu masuk proses kirim besok
+      - Sabtu & Minggu atau Tanggal merah: Off Service (Admin dan Pengiriman libur)
+      - Estimasi tiba Pengiriman Barang Tergantung Ekspedisi Dan Tanggung Jawab Ekspedisi
+      
+      Keluhan Barang :
+      - Bintang 1 Tidak Akan Menyelesaikan Masalah, Apabila Ada Produk Kami Yang Bermasalah, Salah Kirim Atau Cacat
+      Mohon Diinfokan Kepada Kami Langsung Via Chat Dengan Bukti Yang Ada.
+      
+      Penukaran Barang :
+      - Tidak menerima penukaran barang, terkecuali dari kita memang ada barang cacat atau kesalahan kirim
+
+      Return / Komplain Produk
+
+      Kalau ada produk atau orderan terjadi kendala seperti halnya:
+      Produk cacat
+      Produk kurang lengkap
+      Produk tidak sesuai deskripsi
+      Salah kirim produk (termasuk salah size)
+    `,
     sold: "10RB+ Terjual",
     slug: "o2h-nfl-black-jersey",
   },
@@ -29,6 +121,55 @@ const baseProducts = [
       "/img/products/O2H_Hostel_Embroidery_Baseball_Jersey_Black/1.png",
       "/img/products/O2H_Hostel_Embroidery_Baseball_Jersey_Black/2.png",
     ],
+    deskripsi: `
+      Sizing Measurement
+      (Panjang x Lebar)
+
+      Size I: 72cm x 58cm
+      Size II: 74cm x 60cm
+      Size III: 76cm x 62cm
+      
+      Material:
+      Adidas 180gsm
+      Full Bordir Patch
+      
+      Wajib Dibaca! Membeli = Setuju.
+      
+      Wajib Melakukan Video Unboxing Barang Untuk Memastikan Apabila Produk Kami Cacat Atau Ada Kesalahan Pengiriman
+      
+      Cara Order Barang :
+      - Barang Yang Kami Kirimkan Hanya Yang Kaka Order Sesuai Dari Sistem Bukan Melalui Notes Catatan Atau Chat.
+      - Pastikan No Handphone Dan Alamat Sudah Sesuai, Agar Barang Bisa Sampai Ke Tempat Kaka Tanpa Ada Masalah.
+      
+      Pembatalan Barang :
+      - Apabila Ada Pergantian Warna Atau Ukuran, Mohon Ajukan Batal Apabila Tidak Akan Tetap Dikirimkan Sesuai Pesanan Kaka
+      Dan Tidak Berlaku Bila No Resi Proses Sudah Keluar.
+      
+      Ketersediaan Barang :
+      - Selama size dan warna bisa dipilih berarti barang ready dan siap kirim.
+      - Apabila barang ternyata kosong, akan kami infokan secepatnya
+      
+      Jadwal Pengiriman:
+      - Senin s/d Jumat Maksimal transaksi pukul 15.00 WIB. Lebih dari itu masuk proses kirim besok
+      - Sabtu & Minggu atau Tanggal merah: Off Service (Admin dan Pengiriman libur)
+      - Estimasi tiba Pengiriman Barang Tergantung Ekspedisi Dan Tanggung Jawab Ekspedisi
+      
+      Keluhan Barang :
+      - Bintang 1 Tidak Akan Menyelesaikan Masalah, Apabila Ada Produk Kami Yang Bermasalah, Salah Kirim Atau Cacat
+      Mohon Diinfokan Kepada Kami Langsung Via Chat Dengan Bukti Yang Ada.
+      
+      Penukaran Barang :
+      - Tidak menerima penukaran barang, terkecuali dari kita memang ada barang cacat atau kesalahan kirim
+      Return / Komplain Produk
+      
+      Kalau ada produk atau orderan terjadi kendala seperti halnya:
+      Produk cacat
+      Produk kurang lengkap
+      Produk tidak sesuai deskripsi
+      Salah kirim produk (termasuk salah size)
+      
+      Bisa ditanyakan terlebih dahulu via personal chat toko dan juga SERTAKAN bukti video unboxing dan foto saat barang diterima dari kurir. Jikalau 2 syarat ini tidak terpenuhi kita ga bisa proses apapun yang terjadi. Jadi di mohon dengan sangat untuk di simak yah!!.
+    `,
     sold: "10RB+ Terjual",
     slug: "o2h-hostel-embroidery-baseball-jersey-black",
   },
@@ -37,6 +178,49 @@ const baseProducts = [
     price: "Rp. 165.000",
     label: ["Sale", "Hats"],
     image: "/img/products/O2H_MILF_Bucket_Hat/1.png",
+    deskripsi: `
+      Sizing Measurement:
+      All size
+      
+      Material:
+      Baby Canvas
+      
+      Wajib Dibaca! Membeli = Setuju.
+      
+      Wajib Melakukan Video Unboxing Barang Untuk Memastikan Apabila Produk Kami Cacat Atau Ada Kesalahan Pengiriman
+      
+      Cara Order Barang :
+      - Barang Yang Kami Kirimkan Hanya Yang Kaka Order Sesuai Dari Sistem Bukan Melalui Notes Catatan Atau Chat.
+      - Pastikan No Handphone Dan Alamat Sudah Sesuai, Agar Barang Bisa Sampai Ke Tempat Kaka Tanpa Ada Masalah.
+      
+      Pembatalan Barang :
+      - Apabila Ada Pergantian Warna Atau Ukuran, Mohon Ajukan Batal Apabila Tidak Akan Tetap Dikirimkan Sesuai Pesanan Kaka
+      Dan Tidak Berlaku Bila No Resi Proses Sudah Keluar.
+      
+      Ketersediaan Barang :
+      - Selama size dan warna bisa dipilih berarti barang ready dan siap kirim.
+      - Apabila barang ternyata kosong, akan kami infokan secepatnya
+      
+      Jadwal Pengiriman:
+      - Senin s/d Jumat Maksimal transaksi pukul 15.00 WIB. Lebih dari itu masuk proses kirim besok
+      - Sabtu & Minggu atau Tanggal merah: Off Service (Admin dan Pengiriman libur)
+      - Estimasi tiba Pengiriman Barang Tergantung Ekspedisi Dan Tanggung Jawab Ekspedisi
+      
+      Keluhan Barang :
+      - Bintang 1 Tidak Akan Menyelesaikan Masalah, Apabila Ada Produk Kami Yang Bermasalah, Salah Kirim Atau Cacat
+      Mohon Diinfokan Kepada Kami Langsung Via Chat Dengan Bukti Yang Ada.
+      
+      Penukaran Barang :
+      - Tidak menerima penukaran barang, terkecuali dari kita memang ada barang cacat atau kesalahan kirim
+      Return / Komplain Produk
+      Kalau ada produk atau orderan terjadi kendala seperti halnya:
+      Produk cacat
+      Produk kurang lengkap
+      Produk tidak sesuai deskripsi
+      Salah kirim produk (termasuk salah size)
+      
+      Bisa ditanyakan terlebih dahulu via personal chat toko dan juga SERTAKAN bukti video unboxing dan foto saat barang diterima dari kurir. Jikalau 2 syarat ini tidak terpenuhi kita ga bisa proses apapun yang terjadi. Jadi di mohon dengan sangat untuk di simak yah!!.    
+    `,
     sold: "10RB+ Terjual",
     slug: "o2h-milf-bucket-hat",
   },
@@ -48,6 +232,53 @@ const baseProducts = [
       "/img/products/O2H_MILF_Vest/1.png",
       "/img/products/O2H_MILF_Vest/2.png",
     ],
+    deskripsi: `
+      Sizing measurement vest
+      Panjang x lebar
+      
+      Size 1: 65cm x 55cm
+      Size 2: 67cm x 58cm
+      Size 3: 69cm x 61cm
+      
+      Material:
+      Ripstop
+      
+      Wajib Dibaca! Membeli = Setuju.
+      
+      Wajib Melakukan Video Unboxing Barang Untuk Memastikan Apabila Produk Kami Cacat Atau Ada Kesalahan Pengiriman
+      
+      Cara Order Barang :
+      - Barang Yang Kami Kirimkan Hanya Yang Kaka Order Sesuai Dari Sistem Bukan Melalui Notes Catatan Atau Chat.
+      - Pastikan No Handphone Dan Alamat Sudah Sesuai, Agar Barang Bisa Sampai Ke Tempat Kaka Tanpa Ada Masalah.
+      
+      Pembatalan Barang :
+      - Apabila Ada Pergantian Warna Atau Ukuran, Mohon Ajukan Batal Apabila Tidak Akan Tetap Dikirimkan Sesuai Pesanan Kaka
+      Dan Tidak Berlaku Bila No Resi Proses Sudah Keluar.
+      
+      Ketersediaan Barang :
+      - Selama size dan warna bisa dipilih berarti barang ready dan siap kirim.
+      - Apabila barang ternyata kosong, akan kami infokan secepatnya
+      
+      Jadwal Pengiriman:
+      - Senin s/d Jumat Maksimal transaksi pukul 15.00 WIB. Lebih dari itu masuk proses kirim besok
+      - Sabtu & Minggu atau Tanggal merah: Off Service (Admin dan Pengiriman libur)
+      - Estimasi tiba Pengiriman Barang Tergantung Ekspedisi Dan Tanggung Jawab Ekspedisi
+      
+      Keluhan Barang :
+      - Bintang 1 Tidak Akan Menyelesaikan Masalah, Apabila Ada Produk Kami Yang Bermasalah, Salah Kirim Atau Cacat
+      Mohon Diinfokan Kepada Kami Langsung Via Chat Dengan Bukti Yang Ada.
+      
+      Penukaran Barang :
+      - Tidak menerima penukaran barang, terkecuali dari kita memang ada barang cacat atau kesalahan kirim
+      Return / Komplain Produk
+      Kalau ada produk atau orderan terjadi kendala seperti halnya:
+      Produk cacat
+      Produk kurang lengkap
+      Produk tidak sesuai deskripsi
+      Salah kirim produk (termasuk salah size)
+      
+      Bisa ditanyakan terlebih dahulu via personal chat toko dan juga SERTAKAN bukti video unboxing dan foto saat barang diterima dari kurir. Jikalau 2 syarat ini tidak terpenuhi kita ga bisa proses apapun yang terjadi. Jadi di mohon dengan sangat untuk di simak yah!!.
+    `,
     sold: "10RB+ Terjual",
     slug: "o2h-milf-vest",
   },
@@ -97,7 +328,7 @@ const baseProducts = [
 
       Jadwal Pengiriman
       -Pengiriman setiap hari
-      -Senin hingga Sabtu: pesanan sebelum 14.00 WIB dikirim hari yang sama
+      -Senin s/d Sabtu: pesanan sebelum 14.00 WIB dikirim hari yang sama
       -Minggu: pesanan sebelum 13.00 WIB dikirim hari yang sama
       -Hari libur nasional tidak ada pengiriman
       -Estimasi tiba tergantung ekspedisi
