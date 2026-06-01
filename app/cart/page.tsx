2501a4c439edaaa5ae1057b7559a519a8eb9b21f
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2026-06-01 07:45:26 +0700

    Update code

---
+++ app/cart/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "../components/NavbarO2H"
+
+export default function CartPage() {
+  return (
+    <>
+      <title>Cart | O2H Official Site</title>
+      <Navbar />
+    </>
+  )
+}
