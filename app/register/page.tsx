78d0c745f7f04b85a4b0c184170e4991674505c4
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2026-06-10 07:22:15 +0700

    Update code

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,9 @@
+import RegisterContent from "./registerContent";
+
+export const metadata = {
+  title: "Register | O2H Official Site",
+}
+
 export default function RegisterPage() {
-  return (
-    <>
-    
-    </>
-  )
+  return <RegisterContent />
 }
---
+++ app/register/registerContent.tsx
@@ -0,0 +1,9 @@
+export default function RegisterContent() {
+  return (
+    <>
+      <section>
+        
+      </section>
+    </>
+  )
+}
